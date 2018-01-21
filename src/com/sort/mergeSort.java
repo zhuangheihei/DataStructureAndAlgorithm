95b22a9e1dd303197a97316e83423ea879ac1be2
Hong Zhuang <zhuanghong@main-10-230-121-160.tamulink.tamu.edu>
2018-01-21 16:12:43 -0600

    merge sort

--- src/com/sort/mergeSort.java
+++ src/com/sort/mergeSort.java
@@ -5,14 +5,26 @@ public class mergeSort {
     int[] array; //array for store element
     int[] TempArray; //combine
 
+    public static void main(String[] args){
+        int[] a = {1, 5, 2, 7, 3, 6, 0, 4};
+        System.out.println("Before ");
+        for(int num: a){
+            System.out.print(num + "\t");
+        }
+        new mergeSort().prepareForSort(a);
+        System.out.println("\n" + "After");
+        for(int num:a)
+            System.out.print(num + "\t");
+        }
+
     void prepareForSort(int[] arr){
         // prepare for sort
         this.array = arr;
         this.TempArray = new int[arr.length];
-        doMergeSort(0,arr.length-1);
+        doMergeSort(0,arr.length-1); //ex.(1-10)
     }
 
-    //merge two halfs of array
+    //divide array into small sub arrays
     void doMergeSort(int low, int high){
 
         if(low < high){
@@ -23,20 +35,39 @@ public class mergeSort {
         }
     }
 
-    void mergePart(int low, int middle, int high) {
-        
+    //merge and sort each element in small sub array
+    void mergePart(int low, int mid, int high) {
+        for(int i = low; i <= high; i++){
+            TempArray[i] = array[i];    //ex. TempArray = (1-5)
+        }
+        int i = low;
+        int j = mid + 1;
+        int k = low;
+        while(i <= mid && j <= high){
+            if( TempArray[i] <= TempArray[j]){
+                array[k] = TempArray[i];
+                i++;
+            }else {
+                array[k] = TempArray[j];
+                j++;
+            }
+            k++;
+        }
+        //when i or j already reaches the end
+        while(i <= mid){
+            array[k] = TempArray[i];
+            i++;
+            k++;
+        }
+
+        while(j <= high){
+            array[k] = TempArray[j];
+            j++;
+            k++;
+        }
     }
 }
-//    public static void main(String[] args){
-//        int[] a = {1, 5, 2, 7, 3, 6, 0, 4};
-//        System.out.println("Before ");
-//        for(int num: a){
-//            System.out.print(num + "\t");
-//        }
-//        System.out.println("\n" + "After");
-//        mergeSort(a, 0, a.length-1);
-//        for(int num:a){
-//            System.out.print(num + "\t");
-//        }
+
+
 
 
